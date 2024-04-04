c45b37b2242d38c7c4e85722dfbe56ab5ca96362
limch <limch2320@naver.com>
2024-04-04 19:05:23 +0900

    1234

--- backjoon/2557/2557.c
+++ backjoon/2557/2557.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void){
-    printf("Hello World!");
+int main() {
+    int a = 5, b = 7;
+
+    printf("%d == %d: %d\n", a, b, a == b); // 0 (거짓)
+    printf("%d != %d: %d\n", a, b, a != b); // 1 (참)
+    printf("%d > %d: %d\n", a, b, a > b);   // 0 (거짓)
+    printf("%d < %d: %d\n", a, b, a < b);   // 1 (참)
+    printf("%d >= %d: %d\n", a, b, a >= b); // 0 (거짓)
+    printf("%d <= %d: %d\n", a, b, a <= b); // 1 (참)
+
     return 0;
 }
